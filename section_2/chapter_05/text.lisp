a88a08c11d4ed57672a3cae63a3fac2b23c27c79
Clayton Albachten <clay.reed.a@gmail.com>
2014-12-28 18:09:00 -0500

    (look)

--- section_2/chapter_05/text.lisp
+++ section_2/chapter_05/text.lisp
@@ -25,4 +25,30 @@
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 
- 
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+          
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden))) ; another association list 
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+            (eq (cadr (assoc obj obj-locs)) loc)))
+  (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+                    `(you see a ,obj on the floor.)))
+  (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+(defparameter *location* 'living-room) 
+
+(defun look () ; not functional, reads global variables
+               ; not passed into function when called 
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))         
